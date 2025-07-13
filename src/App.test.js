08ed3dd15dfed7aa0ff9c8ec0c79a1fbd1b882ff
Indra <ind_stha@yahoo.com>
2025-07-13 10:58:14 +1000

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,37 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
-
-test('renders profile name', () => {
+test('naviation tabs work correctly',async () => {
   render(<App />);
-  const nameElement = screen.getByText(/Indra Shrestha/i);
-  expect(nameElement).toBeInTheDocument();
+  const introductionTab = screen.getByText(/Introduction/i);
+  const skillsTab = screen.getByText(/Skills/i);
+  const careerTab = screen.getByText(/Experience/i);
+  const educationTab = screen.getByText(/Education/i);
+  const contactTab = screen.getByText(/Contact/i);
+  const profileName = screen.getByText(/Indra Shrestha/i);
+  const jobTitle = screen.getByText(/Full Stack Developer/i);
+  expect(introductionTab).toBeInTheDocument();
+  expect(skillsTab).toBeInTheDocument();
+  expect(careerTab).toBeInTheDocument();
+  expect(educationTab).toBeInTheDocument();
+  expect(contactTab).toBeInTheDocument();
+  expect(profileName).toBeInTheDocument();
+  expect(jobTitle).toBeInTheDocument(); 
+  const user = userEvent.setup();
+  await user.click(introductionTab);
+  expect(screen.getByText(/Introduction/i)).toBeInTheDocument();
+  await user.click(skillsTab);
+  expect(screen.getByText(/Skills/i)).toBeInTheDocument();
+  await user.click(careerTab);
+  expect(screen.getByText(/Experience/i)).toBeInTheDocument();
+  await user.click(educationTab);
+  expect(screen.getByText(/Education/i)).toBeInTheDocument();
+  await user.click(contactTab);
+  expect(screen.getByText(/Contact/i)).toBeInTheDocument();
 });
 
-test('renders job title', () => {
+test('renders ProfileHeader component', () => {
   render(<App />);
-  const jobElement = screen.getByText(/Full Stack Developer/i);
-  expect(jobElement).toBeInTheDocument();
+  const profileHeader = screen.getByText(/Indra Shrestha/i);
+  expect(profileHeader).toBeInTheDocument();
 });
-
